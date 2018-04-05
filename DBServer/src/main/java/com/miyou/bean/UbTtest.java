43acc7cc8e158dc62e1a9daa71b1a2290bfeca6a
stevenlvcrow@163.com <a13537578355>
2018-04-06 00:55:50 +0800

    第二

---
+++ DBServer/src/main/java/com/miyou/bean/UbTtest.java
@@ -0,0 +1,23 @@
+package com.miyou.bean;
+
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+@Entity
+@Data
+public class UbTtest {
+
+    @Id
+    @GeneratedValue
+    String id;
+
+    @Column(name = "name", nullable = true, length = 20)
+    String name;
+
+}
