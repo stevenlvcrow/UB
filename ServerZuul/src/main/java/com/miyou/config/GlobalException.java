79ee1673ef170ea425b825f2cb76c69608dc7e55
stevenlvcrow@163.com <a13537578355>
2018-05-29 14:15:08 +0800

    添加 Swagger2 到zuul

---
+++ ServerZuul/src/main/java/com/miyou/config/GlobalException.java
@@ -0,0 +1,47 @@
+package com.miyou.config;
+
+
+import com.miyou.constant.BusinessConstant;
+import com.miyou.framework.BusinessResponse;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.net.ConnectException;
+
+/**
+　* @Description: contoller所有异常都抛向这里
+　* @author lizhijie
+　* @date 2018/2/7 14:04
+　*/
+@Slf4j
+@ControllerAdvice
+@ResponseStatus(value = HttpStatus.OK)
+@ResponseBody
+public class GlobalException {
+
+
+    @ExceptionHandler(ZuulException.class)
+    public Object exceptionZuulException(Exception e) {
+        log.error("{}" , e);
+        return timeOutRefund();
+    }
+
+    @ExceptionHandler(ConnectException.class)
+    public Object exceptionConnectExceptionException(Exception e) {
+        log.error("{}" , e);
+        return timeOutRefund();
+    }
+
+    private BusinessResponse timeOutRefund(){
+        BusinessResponse errorRes = new BusinessResponse();
+        errorRes.setErrCode(BusinessConstant.ERR_CODE.FATAL);
+        errorRes.setErrInfo("系统连接超时,请稍候再试...");
+        return errorRes;
+    }
+
+}
