eef638ecfaf71b833d62e38d31c7e148dc80392d
stevenlvcrow@163.com <a13537578355>
2018-10-26 09:36:11 +0800

    当前版本保存

--- ServerZuul/src/main/java/com/miyou/controller/web/WebController.java
+++ ServerZuul/src/main/java/com/miyou/controller/web/WebController.java
@@ -6,6 +6,7 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -22,6 +23,7 @@ public class WebController {
     @ApiOperation(value="测试接口", notes="测试通讯是否正常的接口")
     @ApiImplicitParam(name = "postParameters", value = "TestVo", required = true, dataType = "TestVo")
     @PostMapping("/test")
+    @ResponseStatus(value = HttpStatus.OK)
     public Object home(@RequestBody String postParameters) {
         return restTemplate.postForObject(Constant.SERVICEUTL+"/test",new HttpEntity(postParameters, Constant.JSON_HEADER),String.class);
     }
